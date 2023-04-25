38900680e1394cbf7172ce4ec5153ceca33db175
samuele <samuelealbani@gmail.com>
2023-04-25 23:45:20 +0100

    chiasso

---
+++ public/javascript/sketch copy 2.js
@@ -0,0 +1,337 @@
+let song;
+let mic, fft;
+
+let freqCarr = 220;
+let harm2freq = 440;
+let harm3freq = 660;
+
+let freqCarrEnergy;
+let harm2Energy;
+let harm3Energy;
+
+function preload(){
+  song = loadSound('/audio/220.mp3');
+  mic = new p5.AudioIn();
+  mic.start();
+
+  fft = new p5.FFT();
+  fft.setInput(mic);
+}
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+  //----------
+  //the bit between the two comment lines could be move to a three.js sketch except you'd need to create a button there
+  if (typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    // iOS 13+
+    askButton = createButton('Permission');//p5 create button
+    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
+  } else {
+    //if there is a device that doesn't require permission
+    window.addEventListener('devicemotion', deviceMotionHandler, true);
+    window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+  }
+
+  song.play();
+  song.loop();
+
+
+
+
+/*   mic = new p5.AudioIn();
+  mic.start();
+  mic.disconnect();
+
+  fft = new p5.FFT();
+  fft.setInput(mic); */
+}
+
+
+function draw(){
+  background(150);
+
+  
+
+}
+
+function mousePressed() {
+/*   if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  } */
+}
+
+function touchStarted() {
+  if (!audioStarted) {
+    userStartAudio();
+    oscillator.start();
+    audioStarted = true;
+  }
+}
+
+
+function handlePermissionButtonPressed() {
+
+  DeviceMotionEvent.requestPermission()
+    .then(response => {
+      // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+      if (response === 'granted') {
+        mobileDevice = true;
+        window.addEventListener('devicemotion', deviceMotionHandler, true);
+      }
+    });
+
+  DeviceOrientationEvent.requestPermission()
+    .then(response => {
+      if (response === 'granted') {
+        mobileDevice = true;
+        // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+        window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+      }
+    })
+    .catch(console.error);
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicemotion_event
+function deviceMotionHandler(event) {
+
+  /*   accX = event.acceleration.x;
+    accY = event.acceleration.y;
+    accZ = event.acceleration.z;
+  
+    rrateZ = event.rotationRate.alpha;//alpha: rotation around z-axis
+    rrateX = event.rotationRate.beta;//rotating about its X axis; that is, front to back */
+  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
+
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event
+function deviceTurnedHandler(event) {
+
+  //degrees 0 - 365
+  /*   rotateDegrees = event.alpha; // alpha: rotation around z-axis
+    frontToBack = event.beta; // beta: front back motion */
+  leftToRight = event.gamma; // gamma: left to right
+
+}
+
+
+
+/* let oscillator, fft, audioStarted;
+let mic;
+let harmonicFactor;
+let changeNote = false;
+
+let socket;
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+  /// audio
+  oscillator = new p5.Oscillator(); // set frequency and type
+  oscillator.setType('sine');
+  oscillator.amp(1.0);
+  oscillator.freq(400);
+
+  // mic = new p5.AudioIn();
+  // mic.start();
+  // mic.disconnect();
+
+  mic = new p5.AudioIn();
+  mic.disconnect();
+
+  fft = new p5.FFT();
+
+  const myUrl = new URL(window.location.toLocaleString()).searchParams;
+  console.log('myUrl', myUrl);
+  const nMirror = myUrl.getAll('mirror');
+  assignedMirror = nMirror[0];
+  console.log('nMirror:', nMirror, 'assignedMirror:', assignedMirror);
+
+  //----------
+  //the bit between the two comment lines could be move to a three.js sketch except you'd need to create a button there
+  if (typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+    // iOS 13+
+    askButton = createButton('Permission');//p5 create button
+    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
+  } else {
+    //if there is a device that doesn't require permission
+    window.addEventListener('devicemotion', deviceMotionHandler, true);
+    window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+  }
+
+  //----------
+
+  socket = io();
+}
+
+function draw() {
+  background(105);
+
+  if (getAudioContext().state !== 'running') {
+    text('click to start audio', width / 2, height / 2);
+  } else {
+    text('audio is enabled', width / 2, height / 2);
+  }
+
+}
+
+function touchStarted() {
+  if (!audioStarted) {
+    getAudioContext().resume();
+    userStartAudio();
+    oscillator.start();
+    audioStarted = true;
+  }
+
+}
+
+function emit() {
+  socket.emit("data", {
+    address: "/mobileData/" + assignedMirror,
+    args: [
+      { // id
+        type: "s",
+        value: myId
+      },
+      { // assignedMirror
+        type: "i",
+        value: assignedMirror
+      },
+      { // freqCarrEnergy
+        type: "f",
+        value: mouseY / height
+      },
+      { // harm2Energy
+        type: "f",
+        value: harm2Energy
+      },
+      { // harm3Energy
+        type: "f",
+        value: harm3Energy
+      }
+    ]
+  });
+}
+
+//Events that we are listening for
+// Connect to Node.JS Server
+socket.on("connect", () => {
+  console.log(socket.id);
+  myId = socket.id;
+
+  socket.emit("identification", [true, myId]);
+});
+
+// Callback function on the event we disconnect
+socket.on("disconnect", () => {
+  console.log(socket.id);
+});
+
+
+socket.on("setFrequency", setFreq);
+
+socket.on("setHarmonic", setHarm);
+
+
+function setHarm(_harm) {
+  harmonicFactor = _harm;
+  changeNote = true;
+  console.log('received new harmonic factor:', harmonicFactor);
+}
+
+function setFreq(_freq) {
+  if (_freq != currentFeq) {
+    changeNote = true;
+  }
+  currentFeq = _freq;
+  console.log('received new freq', _freq);
+}
+
+//Everything below here you could move to a three.js or other javascript sketch
+function handlePermissionButtonPressed() {
+
+  DeviceMotionEvent.requestPermission()
+    .then(response => {
+      // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+      if (response === 'granted') {
+        mobileDevice = true;
+        window.addEventListener('devicemotion', deviceMotionHandler, true);
+      }
+    });
+
+  DeviceOrientationEvent.requestPermission()
+    .then(response => {
+      if (response === 'granted') {
+        mobileDevice = true;
+        // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+        window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+      }
+    })
+    .catch(console.error);
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicemotion_event
+function deviceMotionHandler(event) {
+
+  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
+
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event
+function deviceTurnedHandler(event) {
+  //degrees 0 - 365
+  leftToRight = event.gamma; // gamma: left to right
+} */
+
+
+/* let mic;
+
+let oscillator;
+
+function setup() {
+  let cnv = createCanvas(100, 100);
+  getAudioContext().suspend();
+
+  // cnv.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  // mic.start();
+  // mic.disconnect();
+
+  oscillator = new p5.Oscillator(); // set frequency and type
+  oscillator.amp(0.5);
+  oscillator.freq(200);
+  oscillator.start();
+  textAlign(CENTER);
+
+ // mic = new p5.AudioIn();
+
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  text('tap to start', width / 2, 20);
+
+  micLevel = mic.getLevel();
+  let y = height - micLevel * height;
+  ellipse(width / 2, y, 10, 10);
+}
+
+function mousePressed() {
+  userStartAudio();
+} */
--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -1,210 +1,88 @@
-let oscillator, fft, audioStarted;
-let mic;
+let freqCarr = 440;
+let tremoloFreq = 442;
+let harm2freq = 660;
+let harm3freq = 880;
+
+let freqCarrEnergy;
+let tremFreqEnergy;
+let harm2Energy;
+let harm3Energy;
+
 let harmonicFactor;
+
+let oscillator, mic, fft;
+
+let currentFeq = 440;
+
+let audioStarted = false;
 let changeNote = false;
 
-let socket;
+let assignedMirror;
+
+// Create connection to Node.JS Server
+const socket = io();
+//global variables
+let askButton;
+
+// device motion
+let accX = 0;
+let accY = 0;
+let accZ = 0;
+let rrateX = 0;
+let rrateY = 0;
+let rrateZ = 0;
+
+// device orientation
+let rotateDegrees = 0;
+let frontToBack = 0;
+let leftToRight = 0;
+
+let mobileDevice = false;
+
+
+// https://github.com/therewasaguy/p5-music-viz/blob/gh-pages/demos/04_fft_freq_spectrum/sketch.js
+var binCount = 1024;
+var bins = new Array(binCount);
+
+let counter;
+
+let song;
+
 
 function preload(){
-  
+  song = loadSound('/audio/440.mp3');
+
 }
 
-function setup() {
+function setup(){
   createCanvas(400, 400);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-
-  /// audio
-  oscillator = new p5.Oscillator(); // set frequency and type
-  oscillator.setType('sine');
-  oscillator.amp(1.0);
-  oscillator.freq(400);
-
-  // mic = new p5.AudioIn();
-  // mic.start();
-  // mic.disconnect();
 
   mic = new p5.AudioIn();
-  mic.disconnect();
+  mic.start();/* 
+  mic.disconnect(); */
 
   fft = new p5.FFT();
+  fft.setInput(mic);
 
-  const myUrl = new URL(window.location.toLocaleString()).searchParams;
-  console.log('myUrl', myUrl);
-  const nMirror = myUrl.getAll('mirror');
-  assignedMirror = nMirror[0];
-  console.log('nMirror:', nMirror, 'assignedMirror:', assignedMirror);
 
-  //----------
-  //the bit between the two comment lines could be move to a three.js sketch except you'd need to create a button there
-  if (typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-    // iOS 13+
-    askButton = createButton('Permission');//p5 create button
-    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
-  } else {
-    //if there is a device that doesn't require permission
-    window.addEventListener('devicemotion', deviceMotionHandler, true);
-    window.addEventListener('deviceorientation', deviceTurnedHandler, true)
-  }
 
-  //----------
+  song.play();
+  song.loop();
 
-  socket = io();
+
 }
 
-function draw() {
-  background(105);
 
-  if (getAudioContext().state !== 'running') {
-    text('click to start audio', width / 2, height / 2);
-  } else {
-    text('audio is enabled', width / 2, height / 2);
-  }
+function draw() {
+
 
 }
 
 function touchStarted() {
   if (!audioStarted) {
-    getAudioContext().resume();
     userStartAudio();
-    oscillator.start();
+    song.play();
     audioStarted = true;
+    console.log("hello");
   }
-
 }
-
-function emit() {
-  socket.emit("data", {
-    address: "/mobileData/" + assignedMirror,
-    args: [
-      { // id
-        type: "s",
-        value: myId
-      },
-      { // assignedMirror
-        type: "i",
-        value: assignedMirror
-      },
-      { // freqCarrEnergy
-        type: "f",
-        value: mouseY / height
-      },
-      { // harm2Energy
-        type: "f",
-        value: harm2Energy
-      },
-      { // harm3Energy
-        type: "f",
-        value: harm3Energy
-      }
-    ]
-  });
-}
-
-//Events that we are listening for
-// Connect to Node.JS Server
-socket.on("connect", () => {
-  console.log(socket.id);
-  myId = socket.id;
-
-  socket.emit("identification", [true, myId]);
-});
-
-// Callback function on the event we disconnect
-socket.on("disconnect", () => {
-  console.log(socket.id);
-});
-
-
-socket.on("setFrequency", setFreq);
-
-socket.on("setHarmonic", setHarm);
-
-
-function setHarm(_harm) {
-  harmonicFactor = _harm;
-  changeNote = true;
-  console.log('received new harmonic factor:', harmonicFactor);
-}
-
-function setFreq(_freq) {
-  if (_freq != currentFeq) {
-    changeNote = true;
-  }
-  currentFeq = _freq;
-  console.log('received new freq', _freq);
-}
-
-//Everything below here you could move to a three.js or other javascript sketch
-function handlePermissionButtonPressed() {
-
-  DeviceMotionEvent.requestPermission()
-    .then(response => {
-      // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
-      if (response === 'granted') {
-        mobileDevice = true;
-        window.addEventListener('devicemotion', deviceMotionHandler, true);
-      }
-    });
-
-  DeviceOrientationEvent.requestPermission()
-    .then(response => {
-      if (response === 'granted') {
-        mobileDevice = true;
-        // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
-        window.addEventListener('deviceorientation', deviceTurnedHandler, true)
-      }
-    })
-    .catch(console.error);
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicemotion_event
-function deviceMotionHandler(event) {
-
-  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
-
-}
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event
-function deviceTurnedHandler(event) {
-  //degrees 0 - 365
-  leftToRight = event.gamma; // gamma: left to right
-}
-
-
-/* let mic;
-
-let oscillator;
-
-function setup() {
-  let cnv = createCanvas(100, 100);
-  getAudioContext().suspend();
-
-  // cnv.mousePressed(userStartAudio);
-  mic = new p5.AudioIn();
-  // mic.start();
-  // mic.disconnect();
-
-  oscillator = new p5.Oscillator(); // set frequency and type
-  oscillator.amp(0.5);
-  oscillator.freq(200);
-  oscillator.start();
-  textAlign(CENTER);
-
- // mic = new p5.AudioIn();
-
-}
-
-function draw() {
-  background(0);
-  fill(255);
-  text('tap to start', width / 2, 20);
-
-  micLevel = mic.getLevel();
-  let y = height - micLevel * height;
-  ellipse(width / 2, y, 10, 10);
-}
-
-function mousePressed() {
-  userStartAudio();
-} */
